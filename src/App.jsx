61811f8083a3f98ff12ac8eed91b1c4bffcb7418
Brett Baker <GitBaBB@MyBaBB.com>
2024-06-30 07:51:00 -0700

    react router installed in working index.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-
+import About from './components/DaveRouter/About'
+import Home from './components/DaveRouter/Home'
+import Test from './components/DaveRouter/Test'
 const App = () => {
   return (
-    <div className='App'>
-      
-    </div>
+    <>
+     
+      <About />
+      <Home />
+      <Test />
+
+
+      </>
   )
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,15 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
-import { createRoot } from 'react-dom'
-import './index.css'
-import App from './App'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+ import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./index.css";
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+   <Router>
+    
+    <Route path="/" component={App} />
 
-createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-    <Router>
-        <Route path='/' component={App} />
     </Router>
-    </React.StrictMode>
+  </React.StrictMode>,
 );
-
- 
