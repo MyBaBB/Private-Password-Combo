628edc1fe4050d76dec91ae979dfb93d16770b9f
Brett Baker <GitBaBB@MyBaBB.com>
2024-06-30 05:14:21 -0700

    changed app to password app everywhere

--- src/Passwordmain.jsx
+++ src/Passwordmain.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/passwordFolder/App"; // Corrected the casing of the file name
+import PasswordApp from "./components/passwordFolder/PasswordApp"; // Corrected the casing of the file name
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <PasswordApp />
   </React.StrictMode>,
 );
