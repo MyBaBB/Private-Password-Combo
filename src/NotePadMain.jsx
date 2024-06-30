95d25c20c77a0f8bb85dd6b6626063e242fb895c
Brett Baker <GitBaBB@MyBaBB.com>
2024-06-30 05:23:35 -0700

    made index.jsx and App in index html for practincing

--- src/NotePadMain.jsx
+++ src/NotePadMain.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import MainWrapper from "./components/MainWrapper/MainWrapper.jsx";
-import { BrowserRouter as Router } from "react-router-dom";
+ 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
+   
     <MainWrapper />
 
 
-    </Router>
+    
   </React.StrictMode>,
 );
