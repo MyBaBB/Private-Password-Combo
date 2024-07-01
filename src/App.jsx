e375d272e5b3632ef474a6e2832e9011a690b1bf
Brett Baker <GitBaBB@MyBaBB.com>
2024-07-01 05:49:12 -0700

    got the paths and the css worked out ,   now I need to figure out the next thing, the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import Missing from './components/DaveRouter/Missing';
 import Nav from './components/DaveRouter/Nav';
 import NewPost from './components/DaveRouter/NewPost';
 import PostPage from './components/DaveRouter/PostPage';
+ import MainWrapper from './components/TodoFolder/WholeTodo'; 
+//  why the css before its called? on the MainWrapper component
+ import Password from './components/passwordFolder/PasswordApp';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 
@@ -17,12 +20,20 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      {/* <Route path="/" element={<Home />} />
       <Route path="/newpost" element={<NewPost />} />
       <Route path="/postpage" element={<PostPage />} />
       <Route path="/about" element={<About />} />
-      <Route path="/missing" element={<Missing />} />
+      <Route path="/footer" element={<Footer />} />
+      <Route path="/nav" element={<Nav />} />
+      <Route path="/header" element={<Header />} />
+      <Route path="/missing" element={<Missing />} /> */}
+      <Route path="/password" element={<Password />} />
+      <Route path="/todo" element={<MainWrapper />} />
+     
+      
       <Route path="/*" element={<Navigate to="/" />} />
+      
     </Routes>
      
            
